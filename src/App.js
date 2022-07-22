b35ed1bfd4df269abd4df5eedd807477b3943562
Joziee <josephine.chivinge@gmail.com>
2022-07-22 12:42:12 +0200

    Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+
+const App = () => {
+  
+  const [people, setPeople] = useState(data);
+
+  return ( 
+    <main>
+      <section className="container">
+        <h3>{ people.length } Birthdays Today</h3>
+        <List people={ people } />
+        { people.length > 0 && <button onClick={ () => { setPeople([]); }}>Clear All</button> }
+        { people.length === 0 && <button onClick={ () => { setPeople(data); }}>Load List</button> }
+      </section>
+    </main>
+   );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const List = () => {
+const List = ( { people } ) => {
   return (
     <>
-      <h2>list component</h2>
+      <h2>list component </h2>   
+      
+      {people.map(person => (
+              <article class="person" key={ person.id }>
+                <img src={person.image} alt={person.name} />
+                <div>
+                    <h4>{person.name}</h4>
+                    <p>{person.age}</p>
+                </div>
+              </article>
+        ))}
     </>
   );
 };
